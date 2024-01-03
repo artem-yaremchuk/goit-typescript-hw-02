0b93d061aabc8f35b76eaf51914a05c781f98b84
artem-yaremchuk <119439325+artem-yaremchuk@users.noreply.github.com>
2024-01-03 19:58:09 +0200

    Feat: Add baseTypes

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -3,5 +3,7 @@
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
 
-let union;
-let literal;
+let union: string | number;
+
+type EnOrDis = "enable" | "disable";
+let literal: EnOrDis;
